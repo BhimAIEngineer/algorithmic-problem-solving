9d86b8816853fa735f8cb4253e8236603727b16f
Bhim Rajbhar <bhimrajbhar.ai.engineer@gmail.com>
2026-09-20 22:51:18 +0530

    Time: 3 ms (97.65%), Space: 19.2 MB (87.39%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -1,3 +1,9 @@
+WEIGHTS = {chr(code): 123 - code for code in range(97, 123)}
+
 class Solution:
     def reverseDegree(self, s: str) -> int:
-        return sum((ord('z') - ord(char) + 1) * i for i, char in enumerate(s, 1))
+        weights = WEIGHTS
+        total = 0
+        for i, char in enumerate(s, 1):
+            total += weights[char] * i
+        return total
